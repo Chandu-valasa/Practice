38fcba482e9738d58efbc9fcb8a2a6660624af25
Chandu-valasa <168898171+Chandu-valasa@users.noreply.github.com>
2025-05-25 19:25:40 +0530

    pyfile

---
+++ samplr.py
@@ -0,0 +1,13 @@
+def chandu(a,b):
+    """
+    This function takes two arguments and returns their sum.
+    
+    Parameters:
+    a (int, float): The first number.
+    b (int, float): The second number.
+    
+    Returns:
+    int, float: The sum of a and b.
+    """
+    return a + b
+chandu
